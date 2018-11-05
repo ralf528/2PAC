a932735f6acd94dfac2c21fca9051551098f28b6
ralf528 <ralf519@naver.com>
2018-11-05 21:41:55 +0900

    새로운 Cpp용 프로젝트 생성

---
+++ trunk/SurvivalProjectCPP/Source/SurvivalProjectCPP/SurvivalProjectCPP.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "SurvivalProjectCPP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SurvivalProjectCPP, "SurvivalProjectCPP" );
+
+DEFINE_LOG_CATEGORY(LogSurvivalProjectCPP)
+ 
---
+++ trunk/SurvivalProjectCPP/Source/SurvivalProjectCPP/SurvivalProjectCPPGameMode.cpp
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "SurvivalProjectCPPGameMode.h"
+#include "SurvivalProjectCPPPlayerController.h"
+#include "SurvivalProjectCPPCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ASurvivalProjectCPPGameMode::ASurvivalProjectCPPGameMode()
+{
+	// use our custom PlayerController class
+	PlayerControllerClass = ASurvivalProjectCPPPlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
