e17f1a1aca2d855ee3d59e83c9ac54b7dbabf3de
ralf528 <ralf519@naver.com>
2018-12-02 01:48:27 +0900

    기존 블루프린트 프로젝트 삭제

--- SurvivalProject/Source/SurvivalProject/SurvivalProject.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "SurvivalProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SurvivalProject, "SurvivalProject" );
